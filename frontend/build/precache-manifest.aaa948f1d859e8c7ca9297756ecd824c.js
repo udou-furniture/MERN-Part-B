83a7ac2ce6274370f6549ddce0ced6075251373e
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-01-17 16:48:01 +1100

    deployed frontend to netlify and setup continuous deployment

---
+++ frontend/build/precache-manifest.aaa948f1d859e8c7ca9297756ecd824c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1f4b87d094a5897ede372a97d86df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa4aff7239319e412c8c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5403f64b5263f951d9aa",
+    "url": "/static/js/2.766069db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.766069db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa4aff7239319e412c8c",
+    "url": "/static/js/main.5ec49011.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
