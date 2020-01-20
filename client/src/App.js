41152377f89ff70918936800f94188554f1239bd
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-01-20 16:09:21 +1100

    added dropdown prototype

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Dropdown from './Dropdown';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!!!!!!!  </h1>
+      <h1>Hello World!!!!!!!!</h1>
+      <Dropdown />
     </div>
   );
 }
