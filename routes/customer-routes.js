75d4f483bebf74d8288fa3f0b0a12d5de1a243c1
Melissa Bykersma <melissabykersma@gmail.com>
2020-01-20 16:16:54 +1100

    removed controllers and put into one file for routes

--- routes/customer-routes.js
+++ routes/customer-routes.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-const {index, create} = require('../controllers/customer-controller')
+const Customer = require('../models/Customer')
 
-router.get('/', index)
-router.post('/create', express.json(), create)
+router.get('/', async (req,res) => {
+    const customers = await Customer.find()
+    res.send(customers)
+})
+
+router.post('/create', express.json(), async (req,res) => {
+    try {
+        const {username, password} = req.body
+        const newCustomer = new Customer({
+            username, 
+            password
+        })
+        const savedCustomer  = await newCustomer.save()
+        res.send(savedCustomer)
+    } catch(err) {
+        res.status(500).send(err.message)
+    }
+})
 
 module.exports = router
