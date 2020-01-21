3e5e6aefbe75103996d3980f6021f3194ed4dd26
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-01-21 15:34:08 +1100

    setup react router and defined first route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 
+
 class App extends React.Component {
   render() {
     return (
