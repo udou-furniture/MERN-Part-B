1053bd5290222f701754aca4f344a76dcc9cdc46
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-01-22 18:57:23 +1100

    created basic collections component

---
+++ client/src/data.js
@@ -0,0 +1,13 @@
+const data = [
+	{type: "bookshelf", name: "Type01"},
+	{type: "bookshelf", name: "Type02"},
+	{type: "bookshelf", name: "Type03"},
+	{type: "wall-unit", name: "Type01"},
+	{type: "wall-unit", name: "Type02"},
+	{type: "wall-unit", name: "Type03"},
+	{type: "sideboard", name: "Type01"},
+	{type: "sideboard", name: "Type02"},
+	{type: "sideboard", name: "Type03"}
+]
+
+
--- client/src/pages/Collections.js
+++ client/src/pages/Collections.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const Collections = () => {
-	return(
-		<div>
-			<h1>Hello from the collections page</h1>
-		</div>
-	)
-}
+class Collections extends React.Component {
+	state = [
+		{type: "bookshelf", name: "Type01"},
+		{type: "bookshelf", name: "Type02"},
+		{type: "bookshelf", name: "Type03"},
+		{type: "wall-unit", name: "Type01"},
+		{type: "wall-unit", name: "Type02"},
+		{type: "wall-unit", name: "Type03"},
+		{type: "sideboard", name: "Type01"},
+		{type: "sideboard", name: "Type02"},
+		{type: "sideboard", name: "Type03"}
+	]
+
+	render () {
+		return (
+			<h1>Sanity check</h1>
+		)
+	}
+}	
 
 export default Collections;
