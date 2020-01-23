d66b33bfc625ef759b8a680ef0dc82abb7d074f4
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-01-23 21:00:35 +1100

    added products list data to products index page

--- client/src/pages/ProductsIndex.js
+++ client/src/pages/ProductsIndex.js
@@ -1,29 +1,80 @@
 import React from 'react';
-import productsList from '../productsList';
+// import productsList from '../productsList';
 
 
-const bookshelves = productsList.filter((product) => {
-	return product.type === "bookshelf"
-})
+	// const bookshelves = productsList.filter((product) => {
+	// return product.type === "bookshelf"
+	// })
 
-// console.log(bookshelves)
+	// const wallunits = productsList.filter((product) => {
+	// return product.type === "wall unit"
+	// })
 
-const wallunits = productsList.filter((product) => {
-	return product.type === "wall unit"
-})
+	// const sideboards = productsList.filter((product) => {
+	// return product.type === "sideboard"
+	// })
 
-const sideboards = productsList.filter((product) => {
-	return product.type === "sideboard"
-})
+
 
 
 class ProductsIndex extends React.Component {
-
-	render() {
-		// console.log(this.props)
+	render() {	
+		const productsList = [
+			{
+				id: 1,
+				type: "bookshelf",
+				name: "Type01"
+			},
+			{
+				id: 2,
+				type: "bookshelf",
+				name: "Type02"
+			},
+			{
+				id: 3,
+				type: "bookshelf",
+				name: "Type03"
+			},
+			{
+				id: 4,
+				type: "wall unit",
+				name: "Type01"
+			},
+			{
+				id: 5,
+				type: "wall unit",
+				name: "Type02"
+			},
+			{
+				id: 6,
+				type: "wall unit",
+				name: "Type03"
+			},
+			{
+				id: 7,
+				type: "sideboard",
+				name: "Type01"
+			},
+			{
+				id: 8,
+				type: "sideboard",
+				name: "Type02"
+			},
+			{
+				id: 9,
+				type: "sideboard",
+				name: "Type03"
+			},
+		
+		];
+		const products = productsList.map((product, key) => 
+			<li key={product.id}>{product.type} {product.name}</li>
+		);
 		return (
 			<div>
-				<h1>Hello from the products index. This page is about to be dynamic</h1>
+				<ul>
+					{products}
+				</ul>	
 			</div>
 		)
 	}
