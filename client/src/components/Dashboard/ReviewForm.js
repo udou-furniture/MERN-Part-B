d762114f9d2a43ebd3e4e6a251fedeba8afab885
Melissa Bykersma <melissabykersma@gmail.com>
2020-02-03 11:40:14 +1100

    validations added to review form

--- client/src/components/Dashboard/ReviewForm.js
+++ client/src/components/Dashboard/ReviewForm.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import {Field, reduxForm} from 'redux-form'
+import {required} from '../../validations'
+
+const renderTextField = ({ input, label, type, meta: { touched, error, warning } }) => (
+    <div>
+        <input {...input} placeholder={label} type={type}/>
+        {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+)
 
 class ReviewForm extends React.Component {
     render() {
-        const {handleSubmit} = this.props
+        const { handleSubmit, pristine, submitting } = this.props
 
         return(
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='review'>Please provide your review response below</label>
-                    <Field name='review' component='input' type='text' />
-                </div>
-                <button type="submit" label='submit'>Submit</button>
+                <label>Please provide your review on our product below</label>
+                <Field 
+                        name="review" 
+                        type="text"
+                        component={renderTextField} 
+                        label="Review"
+                        validate={required}
+                    />
+                <button type="submit" disabled={pristine || submitting}>Submit</button>
             </form>
         )
     }
