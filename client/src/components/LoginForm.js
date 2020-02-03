4a1602007939496cbabf002c74b7178027acaa9a
Melissa Bykersma <melissabykersma@gmail.com>
2020-02-03 12:21:06 +1100

    validations added to login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import {required} from '../validations'
 
+const renderTextField = ({ input, label, type, meta: { touched, error, warning } }) => (
+    <div>
+        <label>{label}</label>
+
+        <div>
+            <input {...input} placeholder={label} type={type}/>
+
+            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+        </div>
+    </div>
+)
 
 class LoginForm extends React.Component {
 	render() {
-        const {handleSubmit} = this.props
+        const { handleSubmit, pristine, submitting } = this.props
 		return (
 				<form onSubmit={handleSubmit}>
-					<div>
-						<label htmlFor="email">Email</label>
-						<Field name="email" component="input" type="email" />
-					</div>
-					<div>
-						<label htmlFor="password">Password</label>
-						<Field name="password" component="input" type="password" />
-					</div>
-					<button type="submit" label='submit'>Submit</button>
+					<Field 
+						name="email" 
+						type="email"
+						component={renderTextField} 
+						label="Email"
+						validate={required}
+				    />
+                    <Field 
+						name="password" 
+						type="password"
+						component={renderTextField} 
+						label="Password"
+						validate={required}
+				    />
+                    {/* <Field 
+						name="passwordConfirmation" 
+						type="password"
+						component={renderTextField} 
+						label="Password"
+						validate={required, matchPassword()}
+				    /> */}
+					<button type="submit" disabled={pristine || submitting}>Submit</button>
 				</form>
 		);
 	}
--- client/src/validations.js
+++ client/src/validations.js
@@ -13,18 +13,20 @@ export const number = value =>
 value && isNaN(Number(value)) ? 'Must be a number' : undefined
 
 
-export const email = value =>
-value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined
-
-
 export const phoneNumber = value =>
-
 value && !/^(?:\+?61|0)4 ?(?:(?:[01] ?[0-9]|2 ?[0-57-9]|3 ?[1-9]|4 ?[7-9]|5 ?[018]) ?[0-9]|3 ?0 ?[0-5])(?: ?[0-9]){5}$/i.test(value)
 ? 'Invalid phone number, must be 10 digits'
 : undefined
 
+export const email = value =>
+value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined
 
+// const passwordsMatch = (password, confirmPassword) => 
+//   password !== confirmPassword
+// ? 'Passwords don\'t match' 
+// : undefined;
 
+// export const matchPassword = passwordsMatch(password, confirmPassword)
 
 
 
