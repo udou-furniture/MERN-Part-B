faf7c7608a915ff78ca54f87410f79f03ae87718
Melissa Bykersma <melissabykersma@gmail.com>
2020-02-05 08:35:40 +1100

    fixed account page tests

--- client/cypress/integration/account_page.spec.js
+++ client/cypress/integration/account_page.spec.js
@@ -8,7 +8,7 @@ describe('Given I am logged in with my account details,', () => {
   })
 
   it('when I try to access Account Dashboard in the navbar, it successfully loads.', () => {
-    cy.contains('a', 'Account Dashboard').click()
+    cy.contains('a', 'My account').click()
 
     cy.location('pathname').should('equal', '/account')
   })
@@ -21,7 +21,7 @@ describe('Given I have purchased furniture on this website previously', () => {
     cy.get('[name=email]').type('melissa@gmail.com')
     cy.get('[name=password]').type('aaaaaaaa')
     cy.contains('button', 'Submit').click()
-    cy.contains('a', 'Account Dashboard').click()
+    cy.contains('a', 'My account').click()
   })
 
   it('when I am on the account dashboard page, then I can see my previously purchased items', () => {
