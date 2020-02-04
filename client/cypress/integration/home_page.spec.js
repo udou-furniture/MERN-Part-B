9fcc926cc923a6fbb67d8165fe534e7a3ae7a5bb
Melissa Bykersma <melissabykersma@gmail.com>
2020-02-04 11:29:42 +1100

    tests done for login and register, started testing for home page

--- client/cypress/integration/home_page.spec.js
+++ client/cypress/integration/home_page.spec.js
@@ -1,5 +1,10 @@
-describe('The Home Page', function() {
-  it('successfully loads', function() {
+describe('Given I am a vistor to the UDOU website', () => {
+  beforeEach(() => {
     cy.visit('/')
+    cy.location('pathname').should('equal', '/')
+  })
+
+  it('when I am on the home page, I can see customer reviews of the furniture.', () => {
+    cy.contains('Customer Reviews')
   })
 })
